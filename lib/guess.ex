00f66baca6e943d16261cc17a13428fa53601d1f
Marcos Schwaab <marcos.schwaab@gmail.com>
2022-05-31 16:21:21 -0300

    add dificult to code

--- lib/guess.ex
+++ lib/guess.ex
@@ -8,5 +8,20 @@ use Application
   
   def run() do
     IO.puts("Let's play Guess the Number")
+    IO.gets("Pick a dificult level 1, 2 or 3):")
+    |> Integer.parse()
+    |> parse_input()
+    |> IO.inspect()
+  end
+
+  def parse_input(data) do
+    if data == :error do
+      IO.puts("Invalid Level!")
+      run()
+    else
+      {num,_} = data
+      num
+    end
   end
 end
+
